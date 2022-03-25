dbcc3290ee9cfeb7e3c6916ef98544fd8b163a92
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-24 21:16:10 -0300

    Adiciona correção em tests :rocket:

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const items = document.querySelector('.items');
 
-function createProductImageElement(imageSource) {
+const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
+};
 
-function createCustomElement(element, className, innerText) {
+const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
-}
+};
 
-function createProductItemElement({ sku, name, image }) {
+const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
   
@@ -24,7 +24,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-}
+};
 
 const getProduct = async () => {
   const funcProducts = await fetchProducts('computador');
@@ -35,21 +35,21 @@ const getProduct = async () => {
   });
 };
 
-function getSkuFromProductItem(item) {
+const getSkuFromProductItem = (item) => {
   return item.querySelector('span.item__sku').innerText;
-}
+};
 
-function cartItemClickListener(event) {
+const cartItemClickListener = (event) => {
   // coloque seu código aqui
-}
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+};
 
 window.onload = async () => { 
   await getProduct();
